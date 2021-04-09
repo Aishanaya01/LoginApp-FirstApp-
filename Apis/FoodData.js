39aa25baed248534264507adaf69bd2726231c9c
Aishanaya01 <aishanayashukla85@gmail.com>
2021-04-09 17:55:54 +0530

    Update a json file

--- Apis/FoodData.js
+++ Apis/FoodData.js
@@ -1,8 +1,20 @@
+import headerImg from "../Images/papaya.jpg";
+
+
 export const FoodData = {
-    Title: "Papaya",
-    OfferPrice: "$5.6",
-    Price: "$10.95",
-    Body:"You Don't Need a Silver Fork To Eat Good Food.",  
-  
-    image: require("../Images/papaya.jpg"),
-  };
+  title: "Papaya",
+  offerPrice: "Rs.2450",
+  price: "Rs.2500",
+  image: headerImg,
+  description:
+    "You Dont Need a Silver Fork To Eat Good Food",
+
+  products: [
+    {
+      productId: "1",
+      productName: "Papaya",
+      productImg: productImg1,
+    },
+    
+  ],
+};
